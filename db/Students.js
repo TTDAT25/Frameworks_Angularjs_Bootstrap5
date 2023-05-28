22f4fc5f1fbaeb578cfab9793b7062f505ef3f47
TTDAT25 <dtruog51@gmail.com>
2023-05-28 13:27:45 +0700

    Project Block 4

---
+++ db/Students.js
@@ -0,0 +1,34 @@
+﻿var listsv=[
+    {
+        "username": "lethanhdat",
+        "password": "123456",
+        "fullname": "Lê Thành Đạt",
+        "email": "datttps21319@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2003-10-25",
+        "schoolfee": "1500000",
+        "marks": "0",
+        "img":"/img/IMG_0515.JPG"
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0",
+        "img":"/img/logoweb.png"
+    },
+    {
+        "username": "nopt",
+        "password": "iloveyou",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
---
+++ db/Subjects.js
@@ -0,0 +1,123 @@
+[
+    {
+        "Id": "ADAV",
+        "Name": "Lập trình Android nâng cao",
+        "Logo": "ADAV.jpg",
+        "Note": "nn"
+    },
+    {
+        "Id": "ADBS",
+        "Name": "Lập trình Android cơ bản",
+        "Logo": "ADBS.jpg",
+        "Note": "nn"
+    },
+    {
+        "Id": "ADTE",
+        "Name": "Kiểm thử và triển khai ứng dụng Android",
+        "Logo": "ADTE.jpg",
+        "Note": "kynang"
+    },
+    {
+        "Id": "ADUI",
+        "Name": "Thiết kế giao diện trên Android",
+        "Logo": "ADUI.jpg",
+        "Note": "Kynang"
+    },
+    {
+        "Id": "ASNE",
+        "Name": "Lập trình ASP.NET",
+        "Logo": "ASNE.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "CLCO",
+        "Name": "Điện toán đám mây",
+        "Logo": "CLCO.jpg",
+        "Note": "csdl"
+    },
+    {
+        "Id": "DBAV",
+        "Name": "SQL Server",
+        "Logo": "DBAV.png",
+        "Note": "csdl"
+    },
+    {
+        "Id": "DBBS",
+        "Name": "Cơ sở dữ liệu",
+        "Logo": "DBBS.png"
+        ,
+        "Note": "csdl"
+    },
+    {
+        "Id": "GAME",
+        "Name": "Lập trình game 2D",
+        "Logo": "GAME.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "HTCS",
+        "Name": "HTML5 và CSS3",
+        "Logo": "HTCS.jpg",
+        "Note": "kynang"
+    },
+    {
+        "Id": "INMA",
+        "Name": "Internet Marketing",
+        "Logo": "INMA.jpg",
+        "Note": "kynang"
+    },
+    {
+        "Id": "JAAV",
+        "Name": "Lập trình Java nâng cao",
+        "Logo": "JAAV.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "JABS",
+        "Name": "Lập trình hướng đối tượng với Java",
+        "Logo": "JABS.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "JSPR",
+        "Name": "Lập trình JavaScript",
+        "Logo": "JSPR.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "LAYO",
+        "Name": "Thiết kế layout",
+        "Logo": "LAYO.jpg",
+        "Note": "kynang"
+    },
+    {
+        "Id": "MOWE",
+        "Name": "Thiết kế web cho điện thoại di động",
+        "Logo": "MOWE.png",
+        "Note": "kynang"
+    },
+    {
+        "Id": "PHPP",
+        "Name": "Lập trình PHP",
+        "Logo": "PHPP.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "PMAG",
+        "Name": "Quản lý dự án với Agile",
+        "Logo": "PMAG.jpg",
+        "Note": "kynang"
+    },
+    {
+        "Id": "VBPR",
+        "Name": "Lập trình VB.NET",
+        "Logo": "VBPR.png",
+        "Note": "nn"
+    },
+    {
+        "Id": "WEBU",
+        "Name": "Xây dựng trang web",
+        "Logo": "WEBU.jpg",
+        "Note": "kynang"
+    }
+]
